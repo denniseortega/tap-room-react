bb2133f2d76a2f0742357711b68cbc59669a81a8
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-29 14:20:03 -0700

    add list of props that the component accepts: name, type, and abv

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -5,11 +5,17 @@ function Keg(props){
   return(
     <div>
       <h3>{props.name} - {props.type}</h3>
-      <p><em>3.7</em></p>
+      <p><em>{props.abv}</em></p>
       <h3>Juice Smoothie ipa</h3>
       <hr/>
     </div>
   );
 }
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+  abv: PropTypes.string
+};
 
 export default Keg;
