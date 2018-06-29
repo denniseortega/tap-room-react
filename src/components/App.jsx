a010da78c264c16e7903b5b3a1c277ae97ab91ae
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-29 12:12:55 -0700

    add header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./Header";
 
 function App(){
-  return ();
+  return (
+    <div> 
+      <h3>Keg 1A</h3>
+      <h3>Modern Times Beer</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </div>
+  );
 }
 
 export default App;
