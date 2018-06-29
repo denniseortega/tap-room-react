ba5aac6b097192271bc9dee7f7773440aab9ad5c
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-29 13:15:55 -0700

    add basic boilerplate to keglist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ function App(){
       <Header/>
       <h3>Keg</h3>
       <h3>Modern Times Beer</h3>
+      <h3>Juice Smoothie ipa</h3>
       <p><em>Firebase entries not saving!</em></p>
       <hr/>
     </div>
