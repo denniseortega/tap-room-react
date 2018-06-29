70b9680105e6a6d280a1241a68123f21db34fc70
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-29 12:03:04 -0700

    add about us info in html

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About(props){
+  return (
+    <div>
+      <h3>Welcome to the tap room! </h3>
+      <p>"We hope you find something you love!"
+</p>
+<h3>Contact Us</h3>
+  <p><strong>Phone:</strong> 1-890-567-5657</p>
+  <p><strong>Email:</strong> taproom@taproom.com</p>
+    </div>
+  );
+}
+
+
