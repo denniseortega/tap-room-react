e6ba71d76a02b243df59fe84183588ca4fb9c61e
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-29 13:14:25 -0700

    render header above hard coded sample keg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,9 @@ import Header from "./Header";
 
 function App(){
   return (
-    <div> 
-      <h3>Keg 1A</h3>
+    <div>
+      <Header/>
+      <h3>Keg</h3>
       <h3>Modern Times Beer</h3>
       <p><em>Firebase entries not saving!</em></p>
       <hr/>
