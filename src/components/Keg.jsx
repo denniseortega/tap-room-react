6ab22751f54f39b8092605cff5945de5f6af43f6
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-29 13:47:02 -0700

    move hard-coded keg from App to Keg

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Keg(){
-  return();
+  return(
+    <div>
+      <Header/>
+      <h3>Keg</h3>
+      <h3>Modern Times Beer</h3>
+      <h3>Juice Smoothie ipa</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </div>
+  );
 }
 
 export default Keg;
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function KegList(){
-  return();
+  return(
+    <Ticket/>
+  );
 }
 
 export default KegList;
