0aba16af2964c465b0eabdd529510c2794299087
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-29 15:39:26 -0700

    add configurations for development tools and development server

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,15 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+
+  devtool: '#source-map',
+
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'build'),
+    publicPath: '/'
+  },
+
   module: {
     rules: [
       {
